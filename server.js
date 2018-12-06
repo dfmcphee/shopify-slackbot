f7c579eaa3cbb2bbf04c88d1e3d95fa6352270f5
Kyle Peatt <kpeatt@gmail.com>
2018-12-06 14:09:20 -0500

    Better unfurling for diff resources

--- server.js
+++ server.js
@@ -18,6 +18,161 @@ const handle = app.getRequestHandler();
 dotenv.config();
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, SHOP_DOMAIN, SLACK_TOKEN, PRIVATE_API_KEY, PRIVATE_API_SECRET_KEY, } = process.env;
 
+function unfurlProduct(product, url) {
+  const productMsg = {};
+  productMsg[url] = {
+    "author_name": "Store Name",
+    "fallback": product.title,
+    "title": product.title,
+    "title_link": url,
+    "fields": [
+      {
+        "title": "Product type",
+        "value": product.product_type,
+        "short": true
+      },
+      {
+        "title": "Vendor",
+        "value": product.vendor,
+        "short": true
+      }
+    ],
+    "actions": [
+      {
+        "type": "button",
+        "text": "View product",
+        "url": url
+      }
+    ],
+    "image_url": product.image.src,
+    "footer": "Store Name"
+  };
+
+  return productMsg;
+}
+
+function unfurlVariant(variant, url) {
+  const variantMsg = {};
+  variantMsg[url] = {
+    "author_name": "Product Name",
+    "fallback": variant.title,
+    "title": variant.title,
+    "title_link": url,
+    "fields": [
+      {
+        "title": "Inventory",
+        "value": variant.inventory_quantity,
+        "short": true
+      },
+    ],
+    "actions": [
+      {
+        "type": "button",
+        "text": "View variant",
+        "url": url
+      }
+    ],
+    "image_url": variant.image_id,
+    "footer": "Store Name"
+  };
+
+  return variantMsg;
+}
+
+function unfurlOrder(order, url) {
+  const orderMsg = {};
+
+  const customerLink = `https://${SHOP_DOMAIN}.myshopify.com/admin/customers/${order.customer.id}`;
+
+  function fulfillmentData(order) {
+    if (order.fulfillment_status == null) {
+      return ":warning: Unfulfilled";
+    } else if (order.fulfillment_status == "partial") {
+      return ":warning: Partially fulfilled";
+    } else if (order.fulfillment_status == "fulfilled") {
+      return ":white_check_mark: Fulfilled";;
+    }
+  }
+
+  function paymentData(order) {
+    if (order.financial_status == "pending") {
+      return ":warning: Pending";
+    } else if (order.financial_status == "paid") {
+      return ":white_check_mark: Paid";
+    } else if (order.financial_status == "unpaid") {
+      return ":warning: Unpaid";
+    } else if (order.financial_status == "partial") {
+      return ":warning: Partially paid";
+    } 
+  }
+
+  function orderNote(order) {
+    if (order.note) {
+      return {
+        title: "Order note",
+        value: order.note
+      }
+    }
+  }
+
+  function orderActions(order) {
+    actions = [
+      {
+        "type": "button",
+        "text": "View order",
+        "url": url
+      }
+    ];
+
+    if (order.fulfillment_status == "partial" || order.fulfillment_status == null) {
+      actions.unshift({
+        "type": "button",
+        "text": "Fulfill order",
+        "style": "primary",
+        "url": url
+      });
+    }
+    
+    return actions;
+  }
+
+  orderMsg[url] = {
+    "author_name": `${order.customer.first_name} ${order.customer.last_name}`,
+    "author_link": customerLink,
+    "fallback": `Order ${order.order_number}`,
+    "title": `Order ${order.order_number}`,
+    "title_link": url,
+    "fields": [
+      orderNote(order),
+      {
+        "title": "Order value",
+        "value": `$${order.total_price}`,
+        "short": true
+      },
+      {
+        "title": "Line items",
+        "value": `${order.line_items.length} items`,
+        "short": true
+      },
+      {
+        "title": "Fulfillment status",
+        "value": fulfillmentData(order),
+        "short": true
+      },
+      {
+        "title": "Payment status",
+        "value": paymentData(order),
+        "short": true
+      }
+    ],
+    "actions": orderActions(order),
+    "footer": "Store Name",
+    "ts": (Date.parse(order.created_at)/1000)
+  };
+
+  return orderMsg;
+}
+
 async function unfurlURL(event, token) {
   const path = url.parse(event.links[0].url).pathname;
   const adminUrl = `https://${PRIVATE_API_KEY}:${PRIVATE_API_SECRET_KEY}@${SHOP_DOMAIN}.myshopify.com${path}.json`;
@@ -25,52 +180,26 @@ async function unfurlURL(event, token) {
   const res = await fetch(adminUrl);
   const json = await res.json();
 
+  let message = {};
+  const itemUrl = event.links[0].url;
+
   console.log(json);
+  
 
-  const productMsg = {};
-  productMsg[event.links[0].url] = {
-      "author_icon": "http://flickr.com/icons/bobby.jpg",
-      "fallback": json.product.title,
-      "title": json.product.title,
-      "title_link": "https://api.slack.com/",
-      "fields": [
-          {
-              "title": "Product type",
-              "value": json.product.product_type,
-              "short": true
-          },
-          {
-              "title": "Vendor",
-              "value": json.product.vendor,
-              "short": true
-          }
-      ],
-      "actions": [
-          {
-              "type": "button",
-              "text": "Start fulfilling",
-              "url": "https://flights.example.com/book/r123456",
-              "style": "primary"
-          },
-          {
-              "type": "button",
-              "text": "View product",
-              "url": event.links[0].url
-          }
-      ],
-      "image_url": json.product.image.src,
-      "thumb_url": "http://example.com/path/to/thumb.png",
-      "footer": "Store Name"
-    };
-
+  if (path.includes("variants")) {
+    message = unfurlVariant(json.variant, itemUrl);
+  } else if (path.includes("products")) {
+    message = unfurlProduct(json.product, itemUrl);
+  } else if (path.includes("orders")) {
+    message = unfurlOrder(json.order, itemUrl);
+  }
+  
   const postData = JSON.stringify({
     token: token,
     channel: event.channel,
     ts: event.message_ts,
-    unfurls: productMsg
+    unfurls: message
   });
-
-  console.log(postData);
 
   const headers = {
     Authorization: `Bearer ${SLACK_TOKEN}`,
@@ -83,8 +212,6 @@ async function unfurlURL(event, token) {
     headers
   });
   const slackJson = await slackRes.json();
-
-  console.log(slackJson);
 }
 
 app.prepare().then(() => {
